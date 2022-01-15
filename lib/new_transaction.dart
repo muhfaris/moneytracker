ecaa8d950a0a22f4555e6e0db2905278454f02ff
Muhammad Faris <muhfaris@disroot.org>
2022-01-15 08:42:58 +0700

    feat: add currency format and dropdown menu (wallet/category)

--- lib/new_transaction.dart
+++ lib/new_transaction.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:moneytracker/custom/currency.dart';
 
 class AddTransaction extends StatefulWidget {
   const AddTransaction({Key? key}) : super(key: key);
@@ -11,11 +13,15 @@ class AddTransaction extends StatefulWidget {
 class _AddTransactionState extends State<AddTransaction> {
   final _keyForm = GlobalKey<FormState>();
   final _amount = TextEditingController();
+  final _currencyFormat = NumberFormat("#,##0", "id");
+
+  String? _selectedWallet = "bca";
+  String? _selectedCategory = "food";
 
   DateTime _now = DateTime.now();
 
-  Future<Null> _selecDate(BuildContext context) async {
-    DateTime? _datePicker = await showDatePicker(
+  Future<Null> _selectDate(BuildContext context) async {
+    final _datePicker = await showDatePicker(
         context: context,
         initialDate: _now,
         firstDate: DateTime(2000),
@@ -52,16 +58,39 @@ class _AddTransactionState extends State<AddTransaction> {
     ));
   }
 
-  Widget buildWalletTyte() => TextFormField(
-        decoration:
-            InputDecoration(labelText: 'Wallet', border: OutlineInputBorder()),
-      );
+  List<DropdownMenuItem<String>> get walletItems {
+    List<DropdownMenuItem<String>> menuItems = [
+      DropdownMenuItem(child: Text("BCA"), value: "bca"),
+      DropdownMenuItem(child: Text("Gopay"), value: "gopay"),
+    ];
+    return menuItems;
+  }
+
+  Widget buildWalletTyte() => DropdownButtonFormField(
+      decoration: InputDecoration(
+        labelText: 'Choose Wallet',
+        border: OutlineInputBorder(),
+      ),
+      value: _selectedWallet,
+      onChanged: (String? newValue) {
+        setState(() {
+          _selectedWallet = newValue;
+        });
+      },
+      items: walletItems);
 
   Widget buildAmount() => TextFormField(
         controller: _amount,
         decoration:
-            InputDecoration(labelText: 'Amount', border: OutlineInputBorder()),
-        keyboardType: TextInputType.number,
+            InputDecoration(
+                prefixText: "Rp ",
+                labelText: 'Amount',
+                border: OutlineInputBorder()),
+        inputFormatters: [
+          FilteringTextInputFormatter.digitsOnly,
+          CurrencyFormat()
+        ],
+        keyboardType: TextInputType.numberWithOptions(decimal: true),
         validator: (value) {
           if (value.toString() == "0") {
             return 'Your amount is empty';
@@ -76,12 +105,27 @@ class _AddTransactionState extends State<AddTransaction> {
             border: OutlineInputBorder()),
         readOnly: true,
         onTap: () {
-            _selecDate(context);
+          FocusScope.of(context).requestFocus(new FocusNode());
+          _selectDate(context);
         },
       );
 
-  Widget buildCategory() => TextFormField(
-        decoration: InputDecoration(
-            labelText: 'Category', border: OutlineInputBorder()),
-      );
+  List<DropdownMenuItem<String>> get categoryItems {
+    List<DropdownMenuItem<String>> menuItems = [
+      DropdownMenuItem(child: Text("Food"), value: "food"),
+      DropdownMenuItem(child: Text("Home"), value: "home"),
+    ];
+    return menuItems;
+  }
+
+  Widget buildCategory() => DropdownButtonFormField(
+      decoration:
+          InputDecoration(labelText: 'Category', border: OutlineInputBorder()),
+      value: _selectedCategory,
+      onChanged: (String? newValue) {
+        setState(() {
+          _selectedCategory = newValue;
+        });
+      },
+      items: categoryItems);
 }
